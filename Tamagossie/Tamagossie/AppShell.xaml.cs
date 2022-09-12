033b7a40c967f5ce6279a9cf4688a3e7ca9dd70b
Rorias <Larsmulderrr@gmail.com>
2022-09-12 17:30:06 +0200

    Initial commit

---
+++ Tamagossie/Tamagossie/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Tamagossie.ViewModels;
+using Tamagossie.Views;
+using Xamarin.Forms;
+
+namespace Tamagossie
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
